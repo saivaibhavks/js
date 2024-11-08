2786d0b5c42ba166254e2d8285943336c7d68a85
Saivaibhav K S (OSV) <saivaibhav.s.osv@fedex.com>
2024-11-08 20:00:03 +0530

    reduce polyfill

--- reduce.js
+++ reduce.js
@@ -3,9 +3,22 @@
 
 const arr = [10, 23, 45, 245];
 
-const sum = arr.reduce((acc, item, index, arr) => {
+
+
+Array.prototype.myReduce = function (cb, initialVal) {
+
+    let accumulator = initialVal;
+
+    for (let i = 0; i < this.length; i++) {
+        accumulator = accumulator ? cb(accumulator, this[i], i, this) : this[i]
+    }
+
+    return accumulator;
+}
+
+const sum = arr.myReduce((acc, item, index, arr) => {
     return acc + item;
 }, 0)
 
 
-console.log("sum is", sum)
+console.log("sum is ", sum)
